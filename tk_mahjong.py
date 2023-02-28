10344db1e5f3b28674303fd60c3a54e2ea126c7f
cha-mic <MITIhisa0223@gmail.com>
2023-03-01 01:35:50 +0900

    add function[button_processing][check]

--- tk_mahjong.py
+++ tk_mahjong.py
@@ -1,6 +1,18 @@
+#計算
+from mahjong.hand_calculating.hand import HandCalculator
+#麻雀牌
+from mahjong.tile import TilesConverter
+#役, オプションルール
+from mahjong.hand_calculating.hand_config import HandConfig, OptionalRules
+#鳴き
+from mahjong.meld import Meld
+#風(場&自)
+from mahjong.constants import EAST, SOUTH, WEST, NORTH
+
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
+
 
 # 手牌の枚数
 tehai_count = 0
@@ -92,15 +104,114 @@ def display_result( m_num, p_num, s_num, j_num ):
     frame2.pack()
     label2.pack(side = TOP)
 
+# チェックボックスの状態を取得
+def check():
 
-# ボタンが押されたら牌を表示する
-def display_hai( type, num):
+    check_state = 0
+
+    for i in range(2):
+        if( kaze_checkvar[i].get() == True ):
+            check_state = check_state + 1
+
+    for i in range(5):
+        if( dora_checkvar[i].get() == True ):
+            check_state = check_state + 1
+
+    for i in range(5):
+        if( uradora_checkvar[i].get() == True ):
+            check_state = check_state + 1
+
+    if( tehai_check.get() == True ):
+        check_state = check_state + 1
+    
+    return check_state
+
+
+# 牌が押された時の処理
+def bottun_processing( type, num):
 
     global tehai_count
     global img_m, img_p, img_s
     global m_num, p_num, s_num, j_num
 
-    if( tehai_button["variable"].get() == True ):
+    wind = {0:EAST, 1:SOUTH, 2:WEST, 3:NORTH}
+
+    dora_indicators = []
+    config = HandConfig(is_riichi=False, player_wind=NONE, round_wind=NONE)
+
+    if( check() != 1 ):
+        print("error")
+    
+    flag = 0
+
+    # 風牌の記録
+    for i in range(2):
+        if( kaze_checkvar[i].get() == True ):
+            place = kaze_canvas[i]
+            
+            flag = 1
+            break
+
+    if( flag == 1 ):
+        place.create_image(4, 4, image = img_j[num], anchor = NW)
+        if( i == 0 ):
+            config.round_wind = wind[num]  
+        elif( i == 1 ):
+            config.player_wind = wind[num] 
+        return 1
+
+    flag = 0  
+
+    # ドラ（表示牌）の記録
+    for i in range(5):
+        if( dora_checkvar[i].get() == True ):
+            place = dora_canvas[i]
+            flag = 1
+            break  
+
+    if( flag == 1 ):
+        if( type == 'm' ):
+            place.create_image(4, 4, image = img_m[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(man = str(num + 1))[0] )
+        elif( type == 'p' ):
+            place.create_image(4, 4, image = img_p[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(pin = str(num + 1))[0] )
+        elif( type == 's' ):
+            place.create_image(4, 4, image = img_s[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(sou = str(num + 1))[0] )
+        elif( type == 'j'):
+            place.create_image(4, 4, image = img_j[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(honors = str(num + 1))[0] )
+        
+        return 1
+
+    flag = 0
+
+    for i in range(5):
+        if( dora_checkvar[i].get() == True ):
+            place = uradora_canvas[i]
+            flag = 1
+            break   
+    
+    if( flag == 1 ):
+        if( type == 'm' ):
+            place.create_image(4, 4, image = img_m[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(man = str(num + 1))[0] )
+        elif( type == 'p' ):
+            place.create_image(4, 4, image = img_p[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(pin = str(num + 1))[0] )
+        elif( type == 's' ):
+            place.create_image(4, 4, image = img_s[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(sou = str(num + 1))[0] )
+        elif( type == 'j'):
+            place.create_image(4, 4, image = img_j[num], anchor = NW)
+            dora_indicators.append( TilesConverter.string_to_136_array(honors = str(num + 1))[0] )
+        
+        return 1
+
+
+
+    if(1):
         if( tehai_count < 13 ):
             if( type == "m" ):
                 tehai_canvas.create_image(tehai_count * 40 + 4 , 4, image = img_m[num], anchor = NW)
@@ -147,6 +258,44 @@ def display_hai( type, num):
     elif( tehai_count > 14 ):
         culc_button["state"] = DISABLED
 
+# 牌の入力ボタンの生成
+def make_inputbutton(row_input):
+
+    # マンズの入力        
+    for mans in range(10):
+        ttk.Button(
+            frame1,
+            image = img_m[mans],
+            command = lambda num = mans: bottun_processing('m', num)
+        ).grid(row = row_input + 1, column = mans, padx = 0)
+
+    # ピンズの入力  
+    for pins in range(10):
+        ttk.Button(
+            frame1,
+            image = img_p[pins],
+            command = lambda num = pins: bottun_processing('p', num)
+        ).grid(row = row_input + 2, column = pins, padx = 0)
+
+    # ソウズの入力  
+    for sous in range(10):
+        ttk.Button(
+            frame1,
+            image = img_s[sous],
+            command = lambda num = sous: bottun_processing('s', num)
+        ).grid(row = row_input + 3, column = sous, padx = 0)
+    
+    # 字牌の入力        
+    for ji in range(7):
+        ttk.Button(
+            frame1,
+            image = img_j[ji],
+            command = lambda num = ji: bottun_processing('j', num)
+        ).grid(row = row_input + 4, column = ji, padx = 0)
+
+    # ここまで
+
+
 
 if __name__ == '__main__':
 
@@ -183,15 +332,19 @@ if __name__ == '__main__':
     frame1.grid(row = 0, column = 0)
     label1.grid(row = 0, column = 0, columnspan = tehai_canvas_width)
 
-    # 風牌，ドラの入力場所
+    # 風牌の入力場所
     row_setting = 3
     column_setting = 0
 
     kaze_button = []
+    kaze_checkvar = []
     kaze_canvas = []
 
-    kaze_button.append( Checkbutton(frame1, text = "場風") )
-    kaze_button.append( Checkbutton(frame1, text = "自風") )
+    kaze_checkvar.append(BooleanVar(value = False))
+    kaze_checkvar.append(BooleanVar(value = False))
+
+    kaze_button.append( Checkbutton(frame1, text = "場風", variable = kaze_checkvar[0]) )
+    kaze_button.append( Checkbutton(frame1, text = "自風", variable = kaze_checkvar[1]) )
 
     kaze_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
     kaze_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
@@ -202,21 +355,29 @@ if __name__ == '__main__':
     kaze_canvas[0].grid(row = row_setting - 1, column = column_setting,     columnspan  =2)
     kaze_canvas[1].grid(row = row_setting - 1, column = column_setting + 2, columnspan  =2)
 
+    # ドラの入力場所
     dora_button = []
+    dora_checkvar = []
     dora_canvas = []
+
     uradora_button = []
+    uradora_checkvar = []
     uradora_canvas = []
 
-    dora_button.append( Checkbutton(frame1, text = "ドラ") )
+    for i in range(5):
+        dora_checkvar.append( BooleanVar(value = False) )
+        uradora_checkvar.append( BooleanVar(value = False) )
+
+    dora_button.append( Checkbutton(frame1, text = "ドラ", variable = dora_checkvar[0]) )
     dora_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
 
-    uradora_button.append( Checkbutton(frame1, text = "裏ドラ") )
+    uradora_button.append( Checkbutton(frame1, text = "裏ドラ", variable = uradora_checkvar[0]) )
     uradora_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
 
     for i in range(4):
-        dora_button.append( Checkbutton(frame1, text = "カンドラ" + str(i + 1)) )
+        dora_button.append( Checkbutton(frame1, text = "カンドラ" + str(i + 1), variable = dora_checkvar[i + 1]) )
         dora_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
-        uradora_button.append( Checkbutton(frame1, text = "裏ドラ" + str(i + 2)) )
+        uradora_button.append( Checkbutton(frame1, text = "裏ドラ" + str(i + 2), variable = uradora_checkvar[i + 1]) )
         uradora_canvas.append( Canvas(frame1, bg = "green", width = 40 , height = 48) )
     
     for i in range(5):
@@ -230,40 +391,7 @@ if __name__ == '__main__':
 
     # 入力ボタンの生成
     row_input = row_setting + 7
-
-    # マンズの入力        
-    for mans in range(10):
-        ttk.Button(
-            frame1,
-            image = img_m[mans],
-            command = lambda num = mans: display_hai('m', num)
-        ).grid(row = row_input + 1, column = mans, padx = 0)
-
-    # ピンズの入力  
-    for pins in range(10):
-        ttk.Button(
-            frame1,
-            image = img_p[pins],
-            command = lambda num = pins: display_hai('p', num)
-        ).grid(row = row_input + 2, column = pins, padx = 0)
-
-    # ソウズの入力  
-    for sous in range(10):
-        ttk.Button(
-            frame1,
-            image = img_s[sous],
-            command = lambda num = sous: display_hai('s', num)
-        ).grid(row = row_input + 3, column = sous, padx = 0)
-    
-    # 字牌の入力        
-    for ji in range(7):
-        ttk.Button(
-            frame1,
-            image = img_j[ji],
-            command = lambda num = ji: display_hai('j', num)
-        ).grid(row = row_input + 4, column = ji, padx = 0)
-
-    # ここまで
+    make_inputbutton( row_input )
 
     # 計算ボタンの配置
     culc_button = ttk.Button(
@@ -274,7 +402,8 @@ if __name__ == '__main__':
         )
     culc_button.grid(row = row_input, column = tehai_canvas_width + 2)
 
-    tehai_button = Checkbutton(frame1, text = "手牌")
+    tehai_check = BooleanVar(value = False)
+    tehai_button = Checkbutton(frame1, text = "手牌", variable = tehai_check)
     tehai_button.grid(row = row_input - 1, column = 0, columnspan = tehai_canvas_width)
     tehai_canvas.grid(row = row_input, column = 0, columnspan = tehai_canvas_width)
     tsumo_canvas.grid(row = row_input, column = tehai_canvas_width + 1)
